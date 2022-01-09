f4b79fef55b4d3ecf27da5399149fb873f776220
Jitendra Agrawal <jitendra@cars24.com>
2022-01-09 13:54:58 +0530

    Add character count implementation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
+  const [charactersRemaining, setCharactersRemaining] = React.useState(280);
+  const [textFontColor, setTextFontColor] = React.useState('black');
+  const handleChangeText = currentText => {
+    let currentLength = currentText.length;
+    if (currentLength >= 280) {
+      setTextFontColor('red');
+    } else {
+      setTextFontColor('black');
+    }
+
+    setCharactersRemaining(280 - currentText.length);
+  };
+
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
-        <Text>Hello world!</Text>
+        <TextInput
+          style={styles.input}
+          multiline={true}
+          onChangeText={handleChangeText}
+          placeholder="Add up 280 characters"
+        />
+        <Text style={{color: textFontColor}}>
+          {charactersRemaining} characters remaining!
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -17,8 +38,15 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  input: {
+    height: 200,
+    borderWidth: 1,
+    padding: 10,
+    width: '90%',
+    marginBottom: 20,
   },
 });
 
